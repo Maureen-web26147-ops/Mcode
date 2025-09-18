b2e23cc67f98f4347a32ef47c5e42851116d49a7
Maureen-web26147-ops <kamau.26147@gmail.com>
2025-09-18 13:27:22 +0300

    Add files via upload

---
+++ newfile.cxx
@@ -0,0 +1,28 @@
+//simple c program(Hello World)single
+/*
+Name:Maureen N Kamau
+Reg:CT100/G/26147/25
+Description:My Profile
+*/
+
+#include<stdio.h>
+
+//main function
+int main()
+{
+      int height;
+      printf("enter your height:");
+      scanf("%d",&height);
+      printf("your height is %d\n",6 );
+      int contact;
+      printf("enter your contact:");
+      scanf("%d",&contact);
+      printf("your contact is%d\n",0727274401);
+      int weight;
+      printf("enter your weight:");
+      scanf("%d",&weight);
+      printf("your weight is %d\n",50 );
+      
+      return 0;
+}            
+      
